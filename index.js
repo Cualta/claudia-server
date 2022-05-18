29417a3660d9576f2180fc38a4ae44c9e63fc609
cualta <dg.claudiahernandez@gmail.com>
2022-05-17 22:56:56 -0400

    full server work

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
 const express = require("express");
-
+const axios = require("axios").default;
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+let webhook = [];
+
+//app.use("/pokemon", require("./pokemonController"));
+
+const fakeMessages = ["hello pokemon name", "you has catch this pokemon"];
+
+setInterval(() => {
+  const message = fakeMessages[Math.floor(Math.random() * 3)];
+  console.log(message);
+  webhook.forEach((url) => {
+    axios.post(url, { message });
+  });
+}, 1000);
 
 app.get("/test", (req, res) => {
-  res.send("is working claudia");
+  res.send("is working Claudia!!!");
 });
 
-app.get("/pokemon", (req, res) => {
-  res.json(pokemon.json);
+app.post("/webhook", (req, res) => {
+  const url = req.query.url;
+  console.log("!!!! Somebody registered a webhook !!!");
+  webhooks.push(url);
+  res.end();
 });
+
+app.delete("/webhook", (req, res) => {
+  const url = req.query.url;
+  webhooks = webhooks.filter((el) => el !== url);
+  res.end();
+});
+
+/* app.get("/pokemon", (req, res) => {
+  res.json("pokemon" + pokemon.json);
+}); */
 
 app.listen(5555, () => {
   console.log("liseting on port 5555");
 });
-
-function getAllPokemon(req, res) {
-  res.json(pokemon);
-}
